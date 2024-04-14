e041bc2d2ee4b2bd86a2489e3b5213db9020b90d
clydefritz <clyde@clydefritz.com>
2024-04-14 15:07:04 -0400

    Dog Barks

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/clydefritz/puppy
 
 go 1.22.2
+
+require github.com/clydefritz/dog v0.0.0-20240414185708-151ba33a3dc6 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,19 @@
 package puppy
 
+import "github.com/clydefritz/dog"
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func DogBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func DogBarks() string {
+	return dog.WhenGrownUp(Barks())
 }
